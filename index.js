645b8343abfb459b128421d7e230db23b1c188aa
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-07-13 19:04:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+
+}
+saturdayFun();
+function mondayWork(toDo = `go to the office`) {
+    return `This Monday, I will ${toDo}.`
+}
+function wrapAdjective(wraps) {
+    return function (adjective = "Special") {
+      return `You are ${wraps}${adjective}${wraps}!`  
+
+    }  
+      
+ }
+
+
+
+
+
+
+
+
+
+
+ 
+  
+
+  
+
+
